b86e6e8e64b41b2eefd18543f34e665ea68b77fa
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-09-10 16:00:56 -0500

    Updated

--- src/components/CreateOrderForm.jsx
+++ src/components/CreateOrderForm.jsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createNewOrder } from "../service/newOrderService";
 
-export const NewOrderForm = () => {
-  const [newOrder, setNewOrder] = useState({
+export const NewOrderForm = ({ currentUser }) => {
+  const navigate = useNavigate();
+  const [order, setOrder] = useState({
+    userId: "",
     customerName: "",
     customerPhoneNumber: "",
     customerEmail: "",
@@ -11,7 +14,48 @@ export const NewOrderForm = () => {
     tip: "",
   });
 
-  
+  const handleNewOrder = (event) => {
+    if (
+      order.delivery === true &&
+      order.customerName &&
+      order.customerPhoneNumber &&
+      order.customerEmail &&
+      order.customerAddress
+    ) {
+      const newOrder = {
+        userId: currentUser.id,
+        customerName: order.customerName,
+        customerPhoneNumber: order.customerPhoneNumber,
+        customerEmail: order.customerEmail,
+        customerAddress: order.customerAddress,
+        delivery: order.delivery,
+        tip: order.tip,
+      };
+      createNewOrder(newOrder).then(() => {
+        navigate("/");
+      });
+    } else if (
+      order.delivery === false &&
+      order.customerName &&
+      order.customerPhoneNumber &&
+      order.customerEmail
+    ) {
+      const newOrder = {
+        userId: currentUser.id,
+        customerName: order.customerName,
+        customerPhoneNumber: order.customerPhoneNumber,
+        customerEmail: order.customerEmail,
+        customerAddress: order.customerAddress,
+        delivery: order.delivery,
+        tip: order.tip,
+      };
+      createNewOrder(newOrder).then(() => {
+        navigate("/");
+      });
+    } else {
+      window.alert("Please fill out fields!");
+    }
+  };
 
   return (
     <form>
@@ -19,42 +63,95 @@ export const NewOrderForm = () => {
       <fieldset>
         <div>
           <label>Name:</label>
-          <input type="text" name="customer-name" />
+          <input
+            type="text"
+            name="customer-name"
+            placeholder="Name"
+            onChange={(event) => {
+              const newOrderCopy = { ...newOrder };
+              newOrderCopy.customerName = event.target.value;
+              setOrder(newOrderCopy);
+            }}
+          />
         </div>
       </fieldset>
       <fieldset>
         <div>
           <label>Phone Number:</label>
-          <input type="text" name="customer-phone" />
+          <input
+            type="text"
+            name="customer-phone"
+            placeholder="Phone Number"
+            onChange={(event) => {
+              const newOrderCopy = { ...newOrder };
+              newOrderCopy.customerPhoneNumber = event.target.value;
+              setOrder(newOrderCopy);
+            }}
+          />
         </div>
       </fieldset>
       <fieldset>
         <div>
           <label>Email:</label>
-          <input type="text" name="customer-email" />
+          <input
+            type="text"
+            name="customer-email"
+            placeholder="Email"
+            onChange={(event) => {
+              const newOrderCopy = { ...newOrder };
+              newOrderCopy.customerEmail = event.target.value;
+              setOrder(newOrderCopy);
+            }}
+          />
         </div>
       </fieldset>
       <fieldset>
         <div>
           <label>Address:</label>
-          <input type="text" name="customer-address" />
+          <input
+            type="text"
+            name="customer-address"
+            placeholder="Address"
+            onChange={(event) => {
+              const newOrderCopy = { ...newOrder };
+              newOrderCopy.customerAddress = event.target.value;
+              setOrder(newOrderCopy);
+            }}
+          />
         </div>
       </fieldset>
       <fieldset>
         <div>
           <label>Delivery?</label>
-          <input type="checkbox" name="delivery-option" />
+          <input
+            type="checkbox"
+            name="delivery-option"
+            onChange={(event) => {
+              const newOrderCopy = { ...newOrder };
+              newOrderCopy.delivery = event.target.checked;
+              setOrder(newOrderCopy);
+            }}
+          />
         </div>
       </fieldset>
       <fieldset>
         <div>
           <label>Tip Amount:</label>
-          <input type="text" name="customer-tip" />
+          <input
+            type="text"
+            name="customer-tip"
+            placeholder="Tip Amount"
+            onChange={(event) => {
+              const newOrderCopy = { ...newOrder };
+              newOrderCopy.tip = event.target.value;
+              setOrder(newOrderCopy);
+            }}
+          />
         </div>
       </fieldset>
       <fieldset>
         <div>
-          <button>Save</button>
+          <button onClick={handleNewOrder}>Save Order</button>
         </div>
       </fieldset>
     </form>
