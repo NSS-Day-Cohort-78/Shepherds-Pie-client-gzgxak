f5c5038fedeb058cae537585150b76961b31f9da
James Heaton <heaton1138@gmail.com>
2025-09-11 13:33:26 -0500

    Employee Edit functionality added

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,5 +1,9 @@
 export const getUserByEmail = (email) => {
-	return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
-		res.json()
-	)
-}
+  return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
+    res.json()
+  );
+};
+
+export const getUsers = () => {
+  return fetch("http://localhost:8088/users").then((res) => res.json());
+};
